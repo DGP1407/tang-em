77ac66b8350280043d99dd4a751d266d09b2b169
DGP1407 <104746803+DGP1407@users.noreply.github.com>
2022-05-02 14:45:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hi baeeee 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi em `,
+    btnIntro: 'OK',
+    title: 'Anh biết Em yêu Anh lắm phải không phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em yêu Anh đó nha :> ',
+    btnYes: 'Cóa, em thật sự rất yêu anh 💖💖💖',
+    btnNo: '💔 Honggg bic, hong bic nữa 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em yêu anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! Bbi',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii 🙄🙄',
+    mess: 'Anh biết mà, Anh cũng yêu em lắm heh 💖',
+    messDesc: 'Hãy cùng chơi game nào :)))',
+    btnAccept: 'Ok lun 💖',
+    
+}
